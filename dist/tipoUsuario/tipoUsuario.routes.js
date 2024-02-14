cf5dfe3b91cdb095a99d99534dbc90a2239683d3
Juani Campora <juanicampora@gmail.com>
2024-02-14 12:35:40 -0300

    eliminados dir y node_modules de git

--- dist/tipoUsuario/tipoUsuario.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { usuarioMethods } from './tipoUsuario.controler.js';
-export const tipoUsuarioRouter = Router();
-tipoUsuarioRouter.post('/add', usuarioMethods.add);
-//# sourceMappingURL=tipoUsuario.routes.js.map
--- dist/usuario/usuario.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { usuarioMethods } from './usuario.controler.js';
-export const usuarioRouter = Router();
-usuarioRouter.get('/:idDj', usuarioMethods.getGmailDj);
-usuarioRouter.post('/login', usuarioMethods.login);
-usuarioRouter.post('/register', usuarioMethods.registerDj);
-usuarioRouter.put('/logout/:uid', usuarioMethods.logout);
-//# sourceMappingURL=usuario.routes.js.map
