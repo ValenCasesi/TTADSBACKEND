0eed1e5133654ce7d3813d7a0789e94e14a8b0ab
valentino <valecasesi@gmail.com>
2024-02-15 20:09:50 -0300

    upload route login

--- src/usuario/usuario.routes.ts
+++ src/usuario/usuario.routes.ts
@@ -7,6 +7,6 @@ export const usuarioRouter = Router()
 
 usuarioRouter.get('/all',usuarioMethods.getAll)
 usuarioRouter.get('/:idDj',usuarioMethods.getGmailDj)
-usuarioRouter.post('/login/:uid/:nombre/:mail',usuarioMethods.login)
+usuarioRouter.post('/login/:uid',usuarioMethods.login)
 usuarioRouter.post('/register',usuarioMethods.registerDj)
 usuarioRouter.put('/logout/:uid',usuarioMethods.logout)
