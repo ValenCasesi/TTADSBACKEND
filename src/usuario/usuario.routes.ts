2905f52bcb16a50e4516b1ecff8e62f52dbce2e6
valentino <valecasesi@gmail.com>
2024-02-15 19:24:51 -0300

    change route login

--- src/usuario/usuario.routes.ts
+++ src/usuario/usuario.routes.ts
@@ -7,6 +7,6 @@ export const usuarioRouter = Router()
 
 usuarioRouter.get('/all',usuarioMethods.getAll)
 usuarioRouter.get('/:idDj',usuarioMethods.getGmailDj)
-usuarioRouter.post('/login',usuarioMethods.login)
+usuarioRouter.post('/login/:uid',usuarioMethods.login)
 usuarioRouter.post('/register',usuarioMethods.registerDj)
 usuarioRouter.put('/logout/:uid',usuarioMethods.logout)
